da9a77270a5ff9a3d890479d3c926453e9ca8afa
tawanchaiii <tawanchai_champ@hotmail.com>
2021-07-28 00:12:51 +0700

    ➕ add countGame.c

--- week5/countGame.c
+++ week5/countGame.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
 int n,k,cnt=0,nub=1,run=0;
 int checkDigit(int x){
-    int temp[4];
-    temp[0] = x%10; x/=10;
-    temp[1] = x%10; x/=10;
-    temp[2] = x%10; x/=10;
-    for(int i=0;i<3;i++) {
-        if (temp[0] == k) return 1;
+    int temp[10];
+    for(int i = 0;i<8;i++) {temp[i] = x%10; x/=10;}
+    for(int i=0;i<8;i++) {
+        if (temp[i] == k) return 1;
     }
     return -1;
 }
@@ -19,14 +14,17 @@ int main(){
     for(int i=0;i<n;i++) scanf("%d",&A[i]);
     while(1){
         if(A[run]>=0){ // simple nub
-            if((nub%k == 0 || checkDigit(nub) == 1) ) A[run]--; // drink
+            if((nub%k == 0 || checkDigit(nub) == 1) ) A[run]--; //drink
             nub++;
             if (A[run]<0) cnt++;
+            
         }
         if(n-cnt == 1) break;
         run++;
-        if (run==n) run = 0;
+        if (run==n) run = 0;    
     }
-    printf("%d",run);
+    for(int i=0;;i++){
+        if (A[i]>=0) {printf("%d",i+1); break;}
+    }
     return 0;
 }
